a9b37658707c5a5b2d98626f0aa5cf3d5f282d65
Poorna Premachandra <poorna.14@cse.mrt.ac.lk>
2018-07-07 11:54:59 +0530

    Update receiver.py
    added comments for clear understanding

--- receiver.py
+++ receiver.py
@@ -1,26 +1,31 @@
 from datetime import datetime, timedelta
 import csv
 
-file= open('Ambewela.txt.','r')
+#use python 2 to run the code
+
+file= open('input.txt.','r')
 fileRead=file.read()
 
 inputArr=fileRead.split('_')
 
+#date array
 dateArr=inputArr[0].split(',')
+
+#precipitation array
 precipitationArr=inputArr[1].split(',')
 
+#An array to convert and save dates data as timestamps
 newDateArr=[]
 
+#conversion function(unix time to utc time)
 def unix_to_utc(unix):
     return (datetime.fromtimestamp(int(unix)/1000) + timedelta(hours=0)).strftime('%Y-%m-%d %H:%M:%S')
     
-for i in dateArr:
-    newDateArr.append(unix_to_utc(i))
+for time in dateArr:
+    newDateArr.append(unix_to_utc(time))
 
-print
-
-
-with open ('Waga.csv','wb') as csvfile:
+#saving the result to a csv file    
+with open ('output.csv','wb') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for x in range(len(newDateArr)):
         filewriter.writerow([newDateArr[x],precipitationArr[x]])
